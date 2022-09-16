cddf82f8958b8e3703015191f81f154a255f0736
GGGemini <tanchikipro7777777@gmail.com>
2022-09-17 01:14:04 +0300

    edit timer

--- js/timer.js
+++ js/timer.js
@@ -1,27 +1,45 @@
-let timeStartMeet = new Date(2020, 1, 13, 19);
-let timeMeetYet = new Date(new Date() - timeStartMeet);
 let timer = document.getElementById("timer");
-let timeTik = 1000;
+
+const startDate = new Date(2020, 1, 13, 0);
+const timeTick = 1000;
+
+let leapYearMonthDays, yearMonthDays, yearsMonthDays;
+leapYearMonthDays = [31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31];
+yearMonthDays = Array.from(leapYearMonthDays);
+leapYearMonthDays.unshift(29);
+yearMonthDays.unshift(28);
+yearsMonthDays = [...leapYearMonthDays, ...yearMonthDays, ...yearMonthDays, ...yearMonthDays];
 
 timer.innerText = getTime();
 
 setInterval(() => {
     timer.innerText = getTime();
-}, timeTik)
+}, timeTick)
 
 function getTime() {
-    timeMeetYet += timeTik;
-
-    let mils = new Date() - timeStartMeet;
-    let seconds = mils / 1000;
+    let milliseconds = new Date() - startDate;
+    let seconds = milliseconds / 1000;
     let minutes = seconds / 60;
     let hours = minutes / 60;
     let days = hours / 24;
-    let month = days / (365.25 / 12);
+    let month = 0;
+
+    for (let i = 0; days >= 28; i++){
+        let monthDay = yearsMonthDays[i];
+        let differenceDay = days - monthDay;
+
+        if (differenceDay >= 0){
+            month++;
+            days = differenceDay;
+        }
+
+        if (i == yearsMonthDays.length - 1){
+            i = 0;
+        }
+    }
 
     let year = month / 12;
     month = month % 12;
-    days = days % (365.25 / 12);
     hours = hours % 24;
     minutes = minutes % 60;
     seconds = seconds % 60;
